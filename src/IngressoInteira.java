f0e75641aa285c3bf1af8f8c895f993c5351e4ec
lobiancovitor <lobiancocnvitor@hotmail.com>
2023-05-12 08:10:53 -0300

    ingresso

--- src/IngressoInteira.java
+++ src/IngressoInteira.java
@@ -1,7 +1,6 @@
 public class IngressoInteira extends Ingresso {
- 
+
     public IngressoInteira(Partida partida, Assento assento, double preco) {
         super(partida, TipoIngresso.INTEIRA, assento, preco);
-        this.preco = 100;
     }
 }
